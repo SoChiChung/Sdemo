a4613c4fb926aa55d9a7a59b488d4b2b925353e4
SoChichung <ddeadwings@gmail.com>
2022-07-19 02:56:22 +0800

    2022.07.19

---
+++ test/testMongo.py
@@ -0,0 +1,27 @@
+"""
+Author: SoChichung
+Date: 2022-07-17 19:57:28
+LastEditors: SoChichung
+LastEditTime: 2022-07-18 20:42:56
+Description: 
+
+Copyright (c) 2022 by SoChichung ddeadwings@gmail.com, All Rights Reserved. 
+"""
+# -*- encoding: utf-8 -*-
+import pymongo
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["runoobdb"]
+
+mycol = mydb["sites"]
+
+item_1 = {
+    "_id": "U1IT00001",
+    "item_name": "Blender",
+    "max_discount": "10%",
+    "batch_number": "RR450020FRG",
+    "price": 340,
+    "category": "kitchen appliance",
+}
+
+mycol.insert_one(item_1)
